963002ca6aca56df3af346a6ab72c4d0d983f3f3
Jay Avgune <avgunejay@gmail.com>
2026-02-27 11:43:37 +0530

    Describe your changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
 
+const count = 0;
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('active');
   menuToggle.classList.toggle('active');
